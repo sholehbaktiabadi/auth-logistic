13ce2f507da80723a48a2da1f87d2c23af708cca
Sholeh Bakti Abadi <45137862+sholehbaktiabadi@users.noreply.github.com>
2021-11-27 10:23:42 +0700

    fix(): import logistic model to app module

--- src/services/logistic/logistic.module.ts
+++ src/services/logistic/logistic.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LogisticService } from './logistic.service';
 import { LogisticController } from './logistic.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Logistic } from './model/logistic.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Logistic])],
   providers: [LogisticService],
   controllers: [LogisticController]
 })
