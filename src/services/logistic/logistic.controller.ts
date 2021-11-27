9f141df5795c6a04633c00d5432456e068c49bcf
Sholeh Bakti Abadi <45137862+sholehbaktiabadi@users.noreply.github.com>
2021-11-27 12:59:13 +0700

    feat(logistic): protected route at logistic route

--- src/services/logistic/logistic.controller.ts
+++ src/services/logistic/logistic.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { LogisticDto } from './dto/logistic.dto';
 import { LogisticService } from './logistic.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('logistic')
 export class LogisticController {
     constructor(private logisticservice: LogisticService){}
